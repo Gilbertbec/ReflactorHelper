419bdd790b4f9f6ef15e5a5c49c4842546034d66
Gilbert <xixi2010@gmail.com>
2018-05-08 13:19:54 -0400

    Clear Code
    Clear Code

--- ReflactorHelper/ReflactorHelper/SelectCaseAttribute.cs
+++ ReflactorHelper/ReflactorHelper/SelectCaseAttribute.cs
@@ -1,20 +1,21 @@
 ﻿/// ReflactorHelper, and with Custom Attribute could remove the switch case in your code 2018-03-02
-/// Gilbert Zhang xixi2010@gamil.com
+/// Gilbert Zhang ca.qc.gilbert@gmail.com
+
 namespace HelpLibrary
 {
-	using System;
+    using System;
 
-	[AttributeUsage(AttributeTargets.Method)]
-	public sealed class ConditionAttribute : System.Attribute
-	{
-		public ConditionAttribute(string functionName, string conditionString)
-		{
-			this.FunctionName = functionName;
-			this.ConditionString = conditionString;
-		}
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class ConditionAttribute : System.Attribute
+    {
+        public ConditionAttribute(string functionName, string conditionString)
+        {
+            this.FunctionName = functionName;
+            this.ConditionString = conditionString;
+        }
 
-		public string FunctionName { get; set; }
+        public string FunctionName { get; set; }
 
-		public string ConditionString { get; set; }
-	}
+        public string ConditionString { get; set; }
+    }
 }
